c1f345b730318414980741faacc8f93c960af8c4
sunchaowang <sunchaowang@outlook.com>
2024-10-25 13:14:43 +0800

    fixed 路径

--- web/functions/api/[[req]].js
+++ web/functions/api/[[req]].js
@@ -1,40 +1,28 @@
 export async function onRequest(context) {
-  // 目标API的URL
-  const targetUrl = context.env.TARGET_URL ||  "https://openai-replay.wochirou.com";
-
-  // 获取原始请求的信息
-  const { request } = context;
+  const {
+    request, // 与现有 Worker API 中的 request 相同
+    env, // 与现有 Worker API 中的 env 相同
+    params, // 如果文件名包含 [id] 或 [[path]]，则与现有 Worker API 中的 params 相同
+    waitUntil, // 与现有 Worker API 中的 ctx.waitUntil 相同
+    next, // 用于中间件或获取资源
+    data, // 在中间件之间传递数据的任意空间
+  } = context;
+  // const newResponse = request.clone();
   const url = new URL(request.url);
-  console.log('request api url: ', url)
+  const headers_Origin = request.headers.get('Access-Control-Allow-Origin') || '*';
 
-  // 创建新的请求URL，包括查询字符串
-  // `url.search` 包含了原始URL中的查询参数，如"?key=value"
-  const newUrl = `${targetUrl}${url.pathname}${url.search || ""}`;
+  // console.log('https://api.openai.com' + url.pathname + url.search);
 
-  // 提取原始请求中的Cookie头
-  const cookies = request.headers.get('Cookie');
-
-  // 准备新请求的头部，包括原始请求中的Cookies
-  const newHeaders = new Headers(request.headers);
-  if (cookies) {
-    newHeaders.set('Cookie', cookies);
-  }
-
-  // 创建新的请求对象，包含新的头部（可能包括Cookies）和方法
-  const newRequestInit = {
-    method: request.method, // 保持原始请求的方法
-    headers: newHeaders,
-    body: request.method !== 'GET' ? request.body : undefined, // GET请求没有body
-    redirect: 'follow' // 根据需要处理重定向
-  };
-
-  // 创建并发送新请求
-  const response = await fetch(newUrl, newRequestInit);
-
-  // 返回目标API的响应
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: response.headers
+  const modifiedRequest = new Request(context.env.TARGET_URL + url.pathname + url.search, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
   });
+
+  const response = await fetch(modifiedRequest);
+
+  const modifiedResponse = new Response(response.body, response);
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+
+  return modifiedResponse;
 }
